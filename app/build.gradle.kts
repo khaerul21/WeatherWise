447eaa5835d7b70af992d8376e4b7b0f7a67b953
Mayko Raditya Wirawan <113871360+maykoRa@users.noreply.github.com>
2024-06-02 21:09:35 +0800

    Perbaikan Error build APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
+        multiDexEnabled = true
         applicationId = "com.example.weatherwise"
         minSdk = 16
         targetSdk = 34
@@ -40,6 +41,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("androidx.multidex:multidex:2.0.1")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
